27657c292dce34661dc9f18c105c1e292d4e9cb6
Stéfano Damiano <stefano.damiano@totvs.com.br>
2018-03-23 16:14:32 -0300

    enable cors

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const cors = require('cors')
 
 const app = express()
 
 const port = process.env.PORT || 8080
 
+app.use(cors())
+
 app.use('/graphql', graphqlHTTP({
-    schema: require('./schemas'),
-    graphiql: true
+  schema: require('./schemas'),
+  graphiql: true
 }))
 
 app.listen(port, (err) => {
-    if(err) {
-        console.error('Error starting server: ', err)
-    } else {
-        console.log(`Server started at port ${port}`)
-    }
+  if (err) {
+    console.error('Error starting server: ', err)
+  } else {
+    console.log(`Server started at port: ${port}`)
+  }
 })
